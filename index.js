b08df0f03a30e285007b7813bdd2366553a7c8fd
Uzair2120 <139527939+huzzi12@users.noreply.github.com>
2023-08-19 23:33:04 +0500

    alldone

---
+++ index.js
@@ -0,0 +1,17 @@
+var myDate = new Date();
+var hrs = myDate.getHours();
+
+var greeter = document.getElementById('greeting')
+var greet;
+
+if (hrs < 12){
+  greet = 'Good Morning';
+}
+else if (hrs >= 12 && hrs <= 17){
+  greet = 'Good Afternoon';
+}
+else if (hrs >= 17 && hrs <= 24){
+    greet = 'Good Evening';
+}
+
+greeter.innerHTML = greet + " " + "Reader!"
--- logout.js
+++ logout.js
@@ -34,4 +34,8 @@ logout.addEventListener('click' , function () {
         console.log(error)
       });
 })
+
+
+
+
     
